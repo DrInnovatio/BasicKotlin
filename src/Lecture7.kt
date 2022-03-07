01e5197d41b70e050b02a2dec5ee955f05852f5c
DrInnovatio <turing1723@gmail.com>
2022-03-07 22:43:19 +0900

    controlling the FOR LOOP.

---
+++ src/Lecture7.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    for(i in 1..10) {
+        if (i == 3) break
+        print("$i ")
+    }
+
+    println()
+
+    for(k in 1..10){
+        if(k == 4) continue
+        print("$k ")
+    }
+    // All numbers are printed except 4.
+
+    println()
+
+    loop@for (i in 1..10){
+        for (j in 1..10){
+            if(i == 1 && j == 2) break@loop
+            println("i : $i, j: $j")
+        }
+    }
+    //Using labels to control the for loop.
+
+}
