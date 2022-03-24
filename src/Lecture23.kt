4e2e5e6aa0764263975d1fd9b6e2ff901750932f
DrInnovatio <turing1723@gmail.com>
2022-03-24 21:29:17 +0900

    The argument of function and infix function.

---
+++ src/Lecture23.kt
@@ -0,0 +1,48 @@
+// The argument of function and infix function
+
+//fun main(){
+//
+//    read(7)
+//    read("WHat is this??")
+//}
+//
+//fun read(x : Int){
+//    println("The number is $x.")
+//}
+//
+//fun read(x : String){
+//    println(x)
+//}
+
+//fun main() {
+//    deliveryItem("Noodle")
+//    deliveryItem("Book", 3)
+//    deliveryItem("Laptop", 30, "School")
+//    deliveryItem("Gift", destination = "friend's house")
+//}
+//
+//fun deliveryItem (name : String, count : Int = 1, destination : String = "House"){
+//    println("${name}, ${count} have beed delivered ${destination}.")
+//}
+
+//fun main() {
+//    sum(1,2,3,4)
+//}
+//
+//fun sum(vararg numbers : Int){
+//    var sum = 0
+//
+//    for(n in numbers){
+//        sum += n
+//    }
+//    print(sum)
+//}
+
+fun main() {
+    println(6 multiply 4)
+    println(6 multiply (4))
+
+}
+
+infix fun Int.multiply(x: Int) =  this * x
+
