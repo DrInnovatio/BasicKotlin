92e7d9734b4519139a8d7898673322e92b9f0c85
DrInnovatio <turing1723@gmail.com>
2022-03-27 22:11:43 +0900

    Collection Function.

---
+++ src/Lecture27.kt
@@ -0,0 +1,27 @@
+// Collection Function
+
+fun main() {
+
+    val nameList = listOf("Park", "James", "Jisoo", "Paul", "Yoona", "Jiwoo")
+
+    nameList.forEach{print("$it ,  ")}
+    println()
+    println(nameList.filter{it.startsWith("Ji")})
+    println()
+    println(nameList.map{"NAME :  $it"})
+    println()
+    println(nameList.any{it == "Paul"})
+    println()
+    println(nameList.all{it.length == 4})
+    println()
+    println(nameList.none{it.startsWith("J")})
+    println()
+    println(nameList.first{it.startsWith("P")})
+    println(nameList.last{it.startsWith("J")})
+    println(nameList.count{it.contains("oo")})
+
+
+
+
+}
+
