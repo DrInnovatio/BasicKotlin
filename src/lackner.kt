3e9590dd9eee1dc4f1b490f38c8bb409b11ccf75
DrInnovatio <turing1723@gmail.com>
2022-04-07 20:09:19 +0900

    Files moved.

---
+++ src/lackner.kt
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=5flXf8nuq60
+
+fun main() {
+
+    /**
+    // Boolean value
+
+    val isHeBrandon = true
+    val isHeDev = false
+    //val guy = isHeBrandon && isHeDev
+    val guy = isHeBrandon || isHeDev
+    println(guy)
+
+    val a = 1
+    val b = 2
+    val c = (a != b)
+    println(c)
+    **/
+
+
+}
