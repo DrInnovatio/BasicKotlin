8d701001ea95ba01b687f4076f914fc2a6b4e369
DrInnovatio <turing1723@gmail.com>
2022-04-09 13:11:29 +0900

    Using readLine().

--- src/lackner.kt
+++ src/lackner.kt
@@ -15,7 +15,29 @@ fun main() {
     val b = 2
     val c = (a != b)
     println(c)
-    **/
+
+
+    val x = 1 + 1
+    val y = if(x == 2) 2 else 3
+    println(y)
+
+
+    // Kotlin is null-safety language.
+
+
+
+    val number1 = readLine() ?: "0"
+    val number2 = readLine() ?: "0"
+    val result = number1.toInt() + number2.toInt()
+    println(result)
+
+     **/
+
+
+
+
+
+
 
 
 
